42ca256078fd33571249cb302d95a74eb1bbceeb
Eduard Kachan <eduard.k.kachan@gmail.com>
2015-04-26 05:05:21 -0400

    server changes

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ var results;
 // declare button pins on Arduino
 var allBtnPin = 2;
 var devBtnPin = 4;
-var desBtnPin = 7;
+var desBtnPin = 6;
+
+var led3;
+var led5;
+var led7;
 
 
 // ------------------------------------
@@ -23,18 +27,18 @@ app.use('/assets/', express.static(__dirname + '/public/assets/'));
 // ------------------------------------
 // Set MySQL Connection
 // ------------------------------------
-app.use(
+// app.use(
 
-    connection(mysql, {
-        host     : 'localhost',
-        user     : 'specialU',
-        password : '5p3c1alP',
-        database : 'special',
-        insecureAuth: true,
-        debug    : false 
-    },'request')
+//     connection(mysql, {
+//         host     : 'localhost',
+//         user     : 'specialU',
+//         password : '5p3c1alP',
+//         database : 'special',
+//         insecureAuth: true,
+//         debug    : false 
+//     },'request')
 
-);
+// );
 
 // ------------------------------------
 // Route to index page
@@ -47,7 +51,7 @@ app.get('/',function(req,res){
 // ------------------------------------
 // Start server
 // ------------------------------------
-http.listen( process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
+http.listen( process.env.PORT || 2000, process.env.IP || "0.0.0.0", function() {
 
 	var addr = http.address();
 	console.log("Server started at", addr.address + ":" + addr.port);
@@ -60,7 +64,19 @@ http.listen( process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
 // ------------------------------------
 socketio.on('connection', function(socket) {
 	socketg = socket;
+
+	socketg.on('stopBlinking', function(data){
+
+	    console.log(data);
+
+	    led3.stop().on();
+		led5.stop().on();
+		led7.stop().on();
+
+	});
 });
+
+console.log('pre board');
 
 // ------------------------------------
 // Arduino Board commands here
@@ -70,23 +86,62 @@ board.on('ready', function(){
 	var devBtn = new jfive.Button(devBtnPin);
 	var desBtn = new jfive.Button(desBtnPin);
 
-	allBtn.on('hit', function(){
-		
-		showAll();
-	}); 
+	console.log('board ready');
 
-	devBtn.on('hit', function(){
-		
+	led3 = new jfive.Led(3);
+	led3.on();
+
+	led5 = new jfive.Led(5);
+	led5.on();
+
+	led7 = new jfive.Led(7);
+	led7.on();
+
+
+	allBtn.on('press', function(){
+
+		console.log('all button pressed');
+
+		led5.stop().on();
+		led7.stop().on();
+		led3.blink(300);
+
+		showAll();
+	});
+
+	devBtn.on('press', function(){
+
+		console.log('dev button pressed');
+
+		led3.stop().on();
+		led7.stop().on();
+		led5.blink(300);
+
 		showDev();
 	});
 
-	desBtn.on('hit', function(){
-		
+	desBtn.on('press', function(){
+
+		console.log('des button pressed');
+
+		led3.stop().on();
+		led5.stop().on();
+		led7.blink(300);
+
 		showDes();
-	});   
+
+	});
 
 	// allBtn.on('release', function(){
-	// 	led.off();
+	// 	led3.off();
+	// });
+
+	// desBtn.on('release', function(){
+	// 	led5.off();
+	// });
+
+	// devBtn.on('release', function(){
+	// 	led7.off();
 	// });
 });
 
